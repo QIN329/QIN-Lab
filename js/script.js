d6c02ef2593be1c141daa058a64fd033f793267a
QIN329 <xgggdfqbbdd@gmail.com>
2026-07-15 17:18:51 +0800

    build Qin Lab entrance interaction

---
+++ js/script.js
@@ -0,0 +1,94 @@
+const cursor = document.querySelector(".cursor");
+
+
+document.addEventListener(
+"mousemove",
+(e)=>{
+
+    cursor.style.left = e.clientX + "px";
+
+    cursor.style.top = e.clientY + "px";
+
+});
+
+
+
+const stars = document.querySelectorAll(".star");
+
+
+document.addEventListener(
+"mousemove",
+(e)=>{
+
+
+    stars.forEach(star=>{
+
+
+        const rect = star.getBoundingClientRect();
+
+
+        const starX = rect.left + rect.width / 2;
+
+
+        const starY = rect.top + rect.height / 2;
+
+
+        const distance = Math.sqrt(
+
+            (e.clientX-starX)**2 +
+
+            (e.clientY-starY)**2
+
+        );
+
+
+
+        if(distance < 130){
+
+            star.classList.add("near");
+
+        }
+
+        else if(!star.classList.contains("found")){
+
+            star.classList.remove("near");
+
+        }
+
+
+    });
+
+
+});
+
+
+
+
+stars.forEach(star=>{
+
+
+    star.addEventListener(
+    "click",
+    ()=>{
+
+        star.classList.add("found");
+
+    });
+
+
+});
+
+
+
+
+
+const polaris = document.querySelector(".polaris");
+
+
+polaris.addEventListener(
+"click",
+()=>{
+
+    document.body.classList.add("entering");
+
+});
